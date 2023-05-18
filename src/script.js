726538d7c8bf3ba8fefd6f7c6b6b70a7d5901b7a
LucasGBarros <Lucasgomesdebarros99@gmail.com>
2023-05-18 15:00:46 -0300

    Teste

--- src/script.js
+++ src/script.js
@@ -36,11 +36,7 @@ document.querySelector('button[type="submit"]')
       return showAndHideErrorMessage()
     }
     if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-<<<<<<< HEAD
       return showAndHideErrorMessage()
-=======
-      // return showAndHideErrorMessage()z
->>>>>>> main
     }
     firstNameField.value = ''
     lastNameField.value = ''
